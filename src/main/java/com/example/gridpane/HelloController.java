5578b3caadbab25a62650a9557aed98597edcbad
ivan <falso@falso.es>
2024-03-07 08:59:08 +0100

    mas apuntes

--- src/main/java/com/example/gridpane/HelloController.java
+++ src/main/java/com/example/gridpane/HelloController.java
@@ -11,4 +11,11 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    /*
+               <image>
+                    <image url= "@imagenes/cafe2.png"> EL @ INICIA EN DONDE ESTÁ EL FXML, EN ESTE CASO EN COM.EXAMPLE.GRIDPANE
+               </image>
+    Image image = new Image (getClass().getResourceAsStream(ImageName));
+               imagenLogo.setImage(image)
+    */
 }
